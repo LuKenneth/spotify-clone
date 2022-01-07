aa3e5ec6b5af8079110903e8bab7ad66e69b778f
Luke Patterson <luke@mandolin.com>
2022-01-07 16:11:46 -0500

    login message

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,10 @@ function Login({ providers }) {
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img className="w-52 mb-5" src="https://i.imgur.com/fPuEa9V.png" alt="" />
       {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
+        <div className="flex items-center flex-col" key={provider.name}>
+          <p className="text-white p-4">
+            You need prior authorization via email by me (Luke) to access
+          </p>
           <button
             className="bg-[#18D860] text-white p-5 rounded-full"
             onClick={() => signIn(provider.id, { callbackUrl: "/" })}
